48dd421c64f10d83a7165a2acb0c6a5a27f1f70f
RegalAdhikari <regalbrt@gmail.com>
2022-12-01 23:59:55 +0545

    time plotted

--- insertionSort/insertion-sort.cpp
+++ insertionSort/insertion-sort.cpp
@@ -2,23 +2,28 @@
 #include<random>
 #include<cstdlib>
 #include<time.h>
+#include<algorithm>
+#include<chrono>
 using namespace std;
+using namespace std::chrono;
 int main()
 {
     int n, j, temp;
     cout<<"\nElements of randomly generated array :\n";
     srand(time(0));
-    n=5;
+    n=100;
     int arr[n];
+    
     for (int i = 0; i < n; i++)
     {   
         arr[i] = rand();
     }
+    
     for (int i = 0; i < n; i++)
     {
         cout<<arr[i] <<" ";
     }
-
+    auto start = high_resolution_clock::now(); //Sorting starts here
     for (int m = 0; m < n; m++)
     {
         for (int i = 0; i < n; i++)
@@ -32,10 +37,12 @@ int main()
             }
         }
     }
+    auto stop = high_resolution_clock::now();
     cout<<"\nSorted: ";
      for (int i = 0; i < n; i++)
     {
         cout<<arr[i]<<" ";
     }
-
+    auto duration = duration_cast<microseconds>(stop - start);
+    cout << "Time taken by function: " << duration.count() << " microseconds" << endl;
 }
